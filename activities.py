be6285ccf7ebd15bd816f501e1a8d871c6bcb569
Rashmi <rashmi@ip-192-168-1-26.ap-south-1.compute.internal>
2024-06-26 19:47:09 +0530

    [chore][activity] create new activity to credit amount into user's account

--- activities.py
+++ activities.py
@@ -1,5 +1,6 @@
 from temporalio import activity
 import requests
+from decimal import Decimal
 
 from shared_objects import EmailDetails, PaymentDetails
 
@@ -16,7 +17,18 @@ async def post_payment(details: PaymentDetails) -> dict:
     print(
         f"Paying {details.amount} via paytm"
     )
-    response = requests.post("http://localhost:3333/payment", json={ "txn_amount": details.amount,"credit_account_id": details.account_id})
+    response = requests.post("http://localhost:3333/payment", json={"txn_amount": str(details.amount),"credit_account_id": details.account_id})
+    print(response.json())
+
+    return response.json()
+
+@activity.defn
+async def account_2_account_credit(payment_confirmation_data: dict) -> dict:
+    print("payment_confirmation_data", payment_confirmation_data)
+    print(
+        f"Crediting to account_id"
+    )
+    response = requests.post("http://localhost:3331/credit", json={"txn_amount": str(payment_confirmation_data.get("txn_amount")),"credit_account_id": payment_confirmation_data.get("account_id")})
     print(response.json())
 
     return response.json()
